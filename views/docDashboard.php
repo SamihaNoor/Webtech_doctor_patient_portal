da271308b1b566a2ee0a20e668a4bac6e37db6fc
Yea Laila Hossain <68175633+yeaonti@users.noreply.github.com>
2020-09-22 20:37:09 +0600

    doctor_updates

---
+++ views/docDashboard.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Doctor Dashboard</title>
+	<link rel="stylesheet" type="text/css" href="../assets/docDashboard.css">
+</head>
+<body>
+	<div>
+		<h1><center>Welcome Doctor!</center></h1>
+		
+	</div>
+
+	<button><center>
+		<a href="notifications.php"> Notifications </a></center>
+	</button>
+
+	<button><center>
+		<a href="appointments.php"> Appointments </a></center>
+		
+	</button>
+
+	<button><center><a href="chat.php"> Chat with Patients </a></center>
+	</button>
+
+	<footer><button>
+		<a href="logout.php"> Log Out</a>
+	</button></footer>
+
+	
+
+
+</body>
+</html>
---
+++ views/notifications.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Notifications</title>
+	
+
+</head>
+<body><form>
+     <center><table border="1">
+     	<tr>
+     		<td>Serial no. </td>
+     		<td>Sender</td>
+     		<td>Message</td>
+     		<td><input type="button" name="" value="mark as read"></td>
+     		<td><input type="button" name="" value="spam"></td>
+     	</tr>
+     	<tr>
+     		<td>01 </td>
+     		<td>Admin</td>
+     		<td>You have been marked as one of the popular doctors..... </td>
+     		<td><input type="button" name="" value="mark as read"></td>
+     		<td><input type="button" name="" value="spam"></td>
+
+     	</tr>
+     	<tr>
+     		<td>02 </td>
+     		<td>Admin</td>
+     		<td>You need to improve your treatment facilities.....</td>
+     		<td><input type="button" name="" value="mark as read"></td>
+     		<td><input type="button" name="" value="spam"></td>
+     		
+     	</tr>
+     	<tr>
+     		<td>03 </td>
+     		<td>Patient</td>
+     		<td>Doctor, I needed to consult with you regarding my diease..... </td>
+     		<td><input type="button" name="" value="mark as read"></td>
+     		<td><input type="button" name="" value="spam"></td>
+     		
+     	</tr>
+
+     </center>
+     </table></form>
+</body>
+</html>
